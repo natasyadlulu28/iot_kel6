6c3c6b9b91afcc7fcdfb4b4500436952ce8d82c7
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2021-01-19 23:36:09 +0700

    Revert "Revert "Minor Update""
    This reverts commit a7663856a606a2c3a945faff68448544ba580bce.

--- iot_kel6.ino
+++ iot_kel6.ino
@@ -1,3 +1,15 @@
+#include <SerialESP8266wifi.h>
+
+#define FIREBASE_HOST "iot-6-d28b7-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "R70A6X5QIK1JcduBGYumU2VrryAjZRJb8lUVhHOb"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+#include <RTClib.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <NTPClient.h>   
+
 int red = 3;
 
 int waterlevel_sensor = A0;
@@ -7,9 +19,14 @@ int waterpump = 12;
 int moisture_sensor = A1;
 int moisture_value = 0;
 
-//map(analogRead(moisture_value), 0, 1023, 0, 100);
-int lembab_rendah;
-int lembab_tinggi;
+int lembab_rendah = 60;
+int lembab_tinggi = 75;
+
+//NTPClient timeClient(ntpUDP, "time.nist.gov", 3600, 60000);
+char Time[] = "TIME:00:00:00";
+char Date[] = "DATE:00/00/2000";
+byte last_second, second_, minute_, hour_, day_, month_;
+int year_;
 
 void setup () {
   pinMode(red,OUTPUT);
@@ -26,26 +43,25 @@ void loop() {
   Serial.print("Kelembaban Tanah: ");
   Serial.print(moisture_value);
   Serial.println("%");
-
-  if(moisture_value < lembab_rendah){
-    Serial.println("KERING, BUTUH AIR!");
-  }else if(moisture_value > lembab_tinggi){
-    Serial.println("BASAH, KEBANYAKAN AIR!");
-  }else{
-    Serial.println("LEMBAB, NICE!^^");
-  }
-//  delay(3000);
-//  if(){
-//    
-//  }else{
-//    
-//  }
+  
   //Persediaan Air
   Serial.print("Persediaan Air: ");
-  if (waterlevel_sensor > 400) {
-    Serial.println("Masih Ada");
-    digitalWrite(red,LOW);
-    delay(1000);
+  if (waterlevel_sensor > 300) {
+     Serial.println("Masih Ada");
+     digitalWrite(red,LOW);
+      if(moisture_value < lembab_rendah){
+        Serial.println("KERING, BUTUH AIR!");
+        digitalWrite(waterpump, LOW); 
+        delay(5000);
+      }else if(moisture_value > lembab_tinggi){
+        digitalWrite(waterpump, HIGH);  
+        Serial.println("BASAH, KEBANYAKAN AIR!");
+        delay(1000);
+      }else{
+        digitalWrite(waterpump, HIGH); 
+        Serial.println("LEMBAB, NICE!^^");
+        delay(1000);
+      }
   }else{
     Serial.println("Habis");
     digitalWrite(red,HIGH);
